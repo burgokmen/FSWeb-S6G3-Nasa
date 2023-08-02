c1f5ecf77f7d1e86267179dab4d7cd599735c28b
burgokmen <burgokmen@gmail.com>
2023-08-02 20:52:24 +0300

    day1 quest completed

---
+++ src/Greetings.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+const Greeting = (props) => {
+  // console.log("props: ", props);
+  // props?.logUser();
+
+  useEffect(() => {
+    // component did mount
+    // console.warn("[ComponentDidMount] Greeting componenti yüklendi!");
+  }, []);
+
+  useEffect(() => {
+    // component did update
+    // console.warn("[ComponentDidUpdate] Greeting componenti yüklendi!");
+  });
+
+  return <h1>Bu Fotoğrafın Çekilme Tarihi ... {props.date}</h1>;
+};
+
+export default Greeting;
